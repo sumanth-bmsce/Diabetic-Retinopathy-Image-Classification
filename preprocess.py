50977a02f9c1a08d7cabaa5ed2c46f8f73e4807d
Sumanth Simha C <ssimha152@gmail.com>
2019-03-26 22:39:35 +0530

    Cropping and Bilinear Scaling of Images

--- preprocess.py
+++ preprocess.py
@@ -1,83 +1,63 @@
 import cv2
 import numpy as np
 from PIL import Image, ImageFilter
+import glob
+import os
+
 
 # Converting images into grayscale
 def convert_to_grayscale(img_arr):
-	gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-	return gray_image
+    gray_image = cv2.cvtColor(img_arr, cv2.COLOR_BGR2GRAY)
+    return gray_image
 
 # Apply Equalize Histogram
-def apply_equalize_histogram(img_arr):
-	eq_image = cv2.equalizeHist(gray_image)
-	return eq_image
+def equalize_hist(img_arr):
 
-# Apply CLAHE
-def apply_CLAHE(img_arr):
-	clahe = cv2.createCLAHE()
-	cl_image = clahe.apply(gray_image)
-	return cl_image
-
-# Filtering Black Images : For black images mean pixel value is 0
-# Under Development
-def filter_black_images(img_arr):
-	if(np.mean(img_arr)==0):
-		return 1
-	else:
-		return 0
+    for c in range(0, 2):
+        img_arr[:,:,c] = cv2.equalizeHist(img_arr[:,:,c])
+    return img_arr
 
 # Cropping black borders of image
 def crop_image(img_arr, tot=0):
-	gray_image = convert_to_grayscale(img_arr)
-	_, thresh = cv2.threshold(gray_image,1,255,cv2.THRESH_BINARY)
-	contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	cnt = contours[0]
-	x,y,w,h = cv2.boundingRect(cnt)
-	#crop = img_arr[y:y+h,x:x+w]
-	crop =  gray_image[y:y+h,x:x+w]
-	return crop
+
+    gray_image = convert_to_grayscale(img_arr)
+    _, thresh = cv2.threshold(gray_image, 1, 255, cv2.THRESH_BINARY)
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnt = contours[0]
+    x, y, w, h = cv2.boundingRect(cnt)
+    # crop = img_arr[y:y+h,x:x+w]
+    crop = img_arr[y:y + h, x:x + w]
+    return crop
 
 # Normal Bilinear Rescaling
-def normal_bilinear_rescaling(img_path_name):
-	img = Image.open(img_path_name)
-	w, h = img.size
-	img = img.resize((w,h), resample = Image.BILINEAR)
-	img.save('bilinear_image.jpeg')
+'''def normal_bilinear_rescaling(src_img_path, dest_img_path, image_name):
+    img = Image.open(image_name)
+    img = img.resize((500, 500), resample=Image.BILINEAR)
+    img.save(dest_img_path + image_name)'''
 
-# Normalizing the image pixels
-def img_normalization(img_arr):
-	mean = np.mean(img_arr)
-	std = np.std(img_arr)
-	print(std)
-	img_arr -= int(mean)
-	img_arr = np.true_divide(img_arr, int(std))
-	return img_arr
-	
+def normal_bilinear_rescaling(img_arr):
+    bilinear_image = cv2.resize(img_arr, (500,500), interpolation = cv2.INTER_LINEAR)
+    return bilinear_image
 
-#blur and unsharp the images
-def blur_and_unsharp_image(img_path_name):
-	img = Image.open(img_path_name)
-	img_blur = img.filter(ImageFilter.GaussianBlur)
-	img_unsharp = img_blur.filter(ImageFilter.UnsharpMask)
-	img_unsharp.save('final_image.jpeg')
-	
+def preprocess(img_path):
+    imgs = glob.glob(img_path + "*.jpeg")
+    img_count = 0
+    names = [os.path.basename(x) for x in imgs]
+    print(names)
+    for imgfile in imgs:
 
-image = cv2.imread('13_left.jpeg')
-# Step 1
-gray_image = convert_to_grayscale(image)
-# Step 2
-cropped_image = crop_image(gray_image)
-cv2.imwrite('cropped_image.jpeg', cropped_image)
-# Step 3
-# saved as bilinear_image.jpeg
-normal_bilinear_rescaling('cropped_image.jpeg')
-# Step 4
-bilinear_image = cv2.imread('bilinear_image.jpeg')
-# Step 5
-clahe_image = apply_CLAHE(bilinear_image)
-cv2.imwrite('clahe_image.jpeg',clahe_image)
-# Step 6
-blur_and_unsharp_image('clahe_image.jpeg')
+        image = cv2.imread(imgfile)
+        # Crop the image to remove the black borders
+        cropped_image = crop_image(image)
+        bilinear_image = normal_bilinear_rescaling(cropped_image)
+        #eq_image = equalize_hist(bilinear_image)
+        #print(imgfile)
+        cv2.imwrite("E:\\DR\\datasets\\preprocessed_dataset\\train001\\" + names[img_count], bilinear_image)
+        img_count += 1
+        if(img_count % 1000 == 0):
+            print(img_count)
 
+img_path = "E:/DR/datasets/original_dataset/train001/"
+preprocess(img_path)
 
 
