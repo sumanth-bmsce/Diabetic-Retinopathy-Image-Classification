8ed52faee71bbbbbf4cd16d18c1cb1ec691f6c36
sumanth-bmsce <ssimha152@gmail.com>
2019-04-12 11:00:47 +0530

    Addition of sample webpage using flask
    Flask for sending form data and retreiving it

---
+++ flask/hello.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def homepage():
+	return render_template('homepage.html')
+
+@app.route("/result", methods = ['POST', 'GET'])
+
+def result():
+	if(request.method=='POST'):
+		result = request.form
+		return render_template('result.html',result=result)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
+
